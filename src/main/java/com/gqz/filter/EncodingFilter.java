918f25399dcea1db69cf04e988936f70681e8fe4
gqz2019 <gqz2019@outlook.com>
2021-09-27 14:05:10 +0800

    添加字符编码过滤器

---
+++ src/main/java/com/gqz/filter/EncodingFilter.java
@@ -0,0 +1,28 @@
+package com.gqz.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * @author gqz20
+ */
+@WebFilter(filterName = "EncodingFilter", value = "/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+
+        chain.doFilter(request, response);
+    }
+}
